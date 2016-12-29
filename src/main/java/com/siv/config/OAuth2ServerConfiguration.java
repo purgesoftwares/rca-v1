2e562b9c4df69940cfe468c11c5ad66eb12f4711
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2016-12-30 00:17:39 +0530

    Increate the access token validity to a valid intiger

--- src/main/java/com/siv/config/OAuth2ServerConfiguration.java
+++ src/main/java/com/siv/config/OAuth2ServerConfiguration.java
@@ -54,7 +54,7 @@ private static final String RESOURCE_ID = "rest_api";
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 			clients.inMemory().withClient("test_client")
 			.authorities("SuperAdmin")
-			.accessTokenValiditySeconds(6000000000)
+			.accessTokenValiditySeconds(6000000)
 			.authorizedGrantTypes("password")
 			.secret("12345")
 			.autoApprove(true)
