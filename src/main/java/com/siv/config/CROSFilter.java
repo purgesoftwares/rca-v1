3d39d8179b5c6255f10bf98aeb3ff7cfe83a7c06
Mamta Soni <mamta.soni@xtreemsolution.com>
2017-01-11 17:41:35 +0530

    update cors specification

--- src/main/java/com/siv/config/CROSFilter.java
+++ src/main/java/com/siv/config/CROSFilter.java
@@ -14,15 +14,10 @@ public class CROSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        final int ACCESS_CONTROL_MAX_AGE_IN_SECONDS = 12 * 60 * 60;
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
         headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("ACCESS_CONTROL_ALLOW_HEADERS", "origin, content-type, accept, authorization");
-        headers.add("ACCESS_CONTROL_ALLOW_CREDENTIALS", "true");
-        headers.add("ACCESS_CONTROL_ALLOW_METHODS", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        headers.add("ACCESS_CONTROL_MAX_AGE", ACCESS_CONTROL_MAX_AGE_IN_SECONDS);
+        headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT"); 
         headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
-
     }
 }
