ff95258998d619c9d1d2913857be156d716be5e3
Shankar Palsaniya <shankar.dayal@purgesoftwares.com>
2017-03-04 12:27:42 +0530

    update exception

--- src/main/java/com/arnav/exceptions/UsernameIsNotAnEmailException.java
+++ src/main/java/com/arnav/exceptions/UsernameIsNotAnEmailException.java
@@ -3,7 +3,7 @@ package com.arnav.exceptions;
 @SuppressWarnings("serial")
 public class UsernameIsNotAnEmailException extends Throwable {
 	
-	public UsernameIsNotAnEmailException(final String message) {
+	public UsernameIsNotAnEmailException(String message) {
         super(message);
     }
 
