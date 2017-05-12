11e41c4b1f45982d59807922edc1ba2602b5f2ab
mamta342 <mamtasoni.jaipur@gmail.com>
2017-05-12 21:33:41 +0530

    Issue resolve of customer pageble

--- src/main/java/com/arnav/repository/coupon/CouponPackageRepository.java
+++ src/main/java/com/arnav/repository/coupon/CouponPackageRepository.java
@@ -1,14 +1,14 @@
 package com.arnav.repository.coupon;
 
-import com.arnav.model.coupon.Coupon;
-import com.arnav.model.coupon.CouponPackage;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.arnav.model.coupon.CouponPackage;
 
 /**
  * Created by Shankar on 2/19/2017.
--- src/main/java/com/arnav/repository/customer/CustomerRepository.java
+++ src/main/java/com/arnav/repository/customer/CustomerRepository.java
@@ -1,15 +1,15 @@
 package com.arnav.repository.customer;
 
-import com.arnav.model.provider.Provider;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.arnav.model.customer.Customer;
 
 @RepositoryRestResource
-public interface CustomerRepository extends CrudRepository<Customer, String>, MongoRepository<Customer, String>{
+public interface CustomerRepository extends MongoRepository<Customer, String>, CrudRepository<Customer, String>, PagingAndSortingRepository<Customer, String>{
 	
 	@Transactional
 	public Customer findByMainEmail(String mainEmail);
